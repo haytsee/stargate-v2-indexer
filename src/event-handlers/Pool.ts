23da54ca22416fb44a0c0cae36219f4155317d81
haytsee <>
2025-08-26 02:54:27 +0300

    Init

---
+++ src/event-handlers/Pool.ts
@@ -0,0 +1,29 @@
+import { StargatePoolNative, StargatePoolUSDC, StargatePoolUSDT } from "generated";
+import { eIdToNetworkId } from "./constants";
+import type {
+  BridgeTransfer,
+  handlerContext as HandlerContext,
+  StargatePoolNative_OFTSent_event as OFTSentEvent,
+} from "generated/src/Types.gen";
+
+type OFTSentArgs = { event: OFTSentEvent; context: HandlerContext };
+
+const handleOFTSent = async (asset: string, args: OFTSentArgs) => {
+  const { event, context } = args;
+
+  let entity: BridgeTransfer = {
+    id: `${event.chainId}-${event.block.number}-${event.logIndex}`,
+    sourceNetworkId: event.chainId,
+    destinationAsset: asset,
+    sourceAsset: asset,
+    amountSent: event.params.amountSentLD,
+    amountReceived: event.params.amountReceivedLD,
+    destinationNetworkId: eIdToNetworkId[Number(event.params.dstEid)] ?? 0,
+  };
+
+  context.BridgeTransfer.set(entity);
+};
+
+StargatePoolNative.OFTSent.handler((args: OFTSentArgs) => handleOFTSent("ETH", args));
+StargatePoolUSDC.OFTSent.handler((args: OFTSentArgs) => handleOFTSent("USDC", args));
+StargatePoolUSDT.OFTSent.handler((args: OFTSentArgs) => handleOFTSent("USDT", args));
---
+++ src/event-handlers/constants.ts
@@ -0,0 +1,5 @@
+export const eIdToNetworkId: Record<number, number> = {
+  30101: 1,
+  30184: 8453,
+  30110: 42161,
+};
